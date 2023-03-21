5b220eb18ed0ca1b737ded73d57f8f926bae1253
NicQ <nicholas.quast2@gmail.com>
2023-03-21 23:57:30 +1100

    privatised struct members and cleaned up + commented header file

--- graph.c
+++ graph.c
@@ -1,2 +1,16 @@
 #include "graph.h"
 
+//Graph Structure
+struct graph {
+    node_t** nodes;
+    int node_num;
+    int node_cpct;
+};
+
+//Node Structure
+struct node{
+    node_t** adj_nodes;
+    int adj_cpct;
+    int adj_num;
+    char id;
+};
--- graph.h
+++ graph.h
@@ -1,26 +1,32 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-//Graph Structure
-typedef struct {
-    node_t** nodes;
-    int node_num;
-    int node_cpct;
-} graph_t;
+typedef struct graph graph_t;
+typedef struct node node_t;
 
-//Node Structure
-typedef struct {
-    node_t** adj_nodes;
-    int adj_cpct;
-    int adj_num;
-    int id;
-} node_t;
-
+//Graph initialisation function
 graph_t* graph_create();
+//Deletion function for graph
+void graph_free(graph_t*);
 
-node_t* graph_add_node(graph_t*, int);
+//Add a node to the graph
+node_t* graph_add_node(graph_t*, char);
+//Remove and deallocate memory for a node in a graph
 void graph_remove_node(graph_t*, node_t*);
+//Gets the address of a node from its ID
+//Returns NULL if one with that ID isn't found in the graph
+node_t* graph_get_node(graph_t*, char);
 
-void add_edge(node_t*, node_t*)
+//Adds an edge between 2 nodes
+void node_add_edge(node_t*, node_t*)
 
+//Returns the number of nodes in the graph
+int graph_size(graph_t*);
+//Returns a list of the addresses of the nodes in the graph
+node_t** graph_nodes(graph_t*);
+
+//Returns a list of nodes adjacent to the input node
+node_t** node_adjacent(node_t*);
+//Gives the number of adjacent nodes of a node
+int node_adjacent_count(node_t*); 
 #endif
